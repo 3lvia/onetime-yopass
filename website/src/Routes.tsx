24adc6cbab0a57804a6b765cf6197412084d6734
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@elvia.no>
2021-08-02 13:29:13 +0200

    core-910: callback experiment

--- website/src/Routes.tsx
+++ website/src/Routes.tsx
@@ -3,13 +3,16 @@ import { Route } from 'react-router-dom';
 import CreateSecret from './createSecret/CreateSecret';
 import DisplaySecret from './displaySecret/DisplaySecret';
 import Upload from './createSecret/Upload';
-import LoginPage from './createSecret/LoginPage';
+import LoginCallback from './authentication/LoginCallback';
+import LoginPage from './authentication/LoginPage';
 
 export const Routes = () => {
   return (
     <div>
       {/* <Route path="/" exact={true} component={CreateSecret} /> */}
       <Route path="/" exact={true} component={LoginPage} />
+      <Route path="/loginCallback" exact={true} component={LoginCallback} />
+      <Route path="/loginPage" exact={true} component={LoginPage} />
       <Route path="/createSecret" exact={true} component={CreateSecret} />
       <Route path="/upload" exact={true} component={Upload} />
       <Route exact={true} path="/s/:key/:password" component={DisplaySecret} />
