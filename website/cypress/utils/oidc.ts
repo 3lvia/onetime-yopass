6dd7fc38525f119d237fe7147599f64d335a0c8a
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@elvia.no>
2021-09-07 16:46:42 +0200

    core-910: fail to automate test user sign with user client

---
+++ website/cypress/utils/oidc.ts
@@ -0,0 +1,26 @@
+Cypress.Commands.add('IdentityServerAPILogin', (email, password) => {
+  cy.intercept();
+  const elvidAuthority = Cypress.env('ELVID_AUTHORITY_URL');
+
+  cy.request('GET', elvidAuthority).then(response => {
+    // Parses the html response to fetch the CSRF token
+    const htmlDocument = document.createElement('html');
+    htmlDocument.innerHTML = response.body;
+    const loginForm = htmlDocument.getElementsByTagName('form')[0];
+    const requestVerificationToken = loginForm.elements.__RequestVerificationToken.value;
+
+    // Sends a valid request to thirdPartyServerUrl which sets the session cookies on a successful response
+    cy.request({
+      method: 'POST',
+      url: elvidAuthority + '/loginEndpoint',
+      followRedirect: false,
+      form: true,
+      body: {
+        ReturnUrl: '',
+        Email: email,
+        Password: password,
+        __RequestVerificationToken: requestVerificationToken
+      }
+    });
+  });
+});
