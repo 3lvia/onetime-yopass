c49efcc3ab06a152da5717211d257048d3187f2a
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@elvia.no>
2021-09-08 12:07:21 +0200

    core-910: initial playwright tests

---
+++ playwright/tests/blank.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+test('blank', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  const description = page.locator('span#blankPageDescription');
+  await expect(description).toHaveText('This page intentionally left blank.');
+
+  const signInOrSignOutButtonTitle = page.locator(
+    'button#signInOrSignOutButton',
+  );
+  await expect(signInOrSignOutButtonTitle).toHaveText('Sign-In');
+});
