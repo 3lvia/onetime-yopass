21be2d1e927d72a15f6cb5b7ce6a26249d741451
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@elvia.no>
2021-09-09 13:11:30 +0200

    core-1036: reuse storage state

--- website/playwright/tests/reuse_storage_state.spec.ts
+++ website/playwright/tests/reuse_storage_state.spec.ts
@@ -11,19 +11,21 @@ test('reuse_storage_state', async ({ page }) => {
     'button#signInOrSignOutButton',
   );
   await expect(signInOrSignOutButtonTitle).toHaveText('Sign-Out');
-  await signInOrSignOutButtonTitle.screenshot({ path: 'tests/output/reuse_storage_state_signinorout_button.png' });
+  await signInOrSignOutButtonTitle.screenshot({
+    path: 'tests/output/reuse_storage_state_signinorout_button.png',
+  });
 
-  const createButtonTitle = page.locator(
-    'a#createButton',
-  );
+  const createButtonTitle = page.locator('a#createButton');
   await expect(createButtonTitle).toHaveText('Create');
-  await createButtonTitle.screenshot({ path: 'tests/output/reuse_storage_state_create_button.png' });
+  await createButtonTitle.screenshot({
+    path: 'tests/output/reuse_storage_state_create_button.png',
+  });
 
-  const uploadButtonTitle = page.locator(
-    'a#uploadButton',
-  );
+  const uploadButtonTitle = page.locator('a#uploadButton');
   await expect(uploadButtonTitle).toHaveText('Upload');
-  await uploadButtonTitle.screenshot({ path: 'tests/output/reuse_storage_state_upload_button.png' });
+  await uploadButtonTitle.screenshot({
+    path: 'tests/output/reuse_storage_state_upload_button.png',
+  });
 
   await page.screenshot({ path: 'tests/output/reuse_storage_state.png' });
 });
