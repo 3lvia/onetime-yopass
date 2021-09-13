61ad91481e44722f5957581893eff7a385c3e1a9
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@elvia.no>
2021-09-13 16:12:45 +0200

    Revert "core-1036: increase timeout for tests"
    This reverts commit 2f79f5027f15fdb45a0f67070b23893bd7cb7859.

--- website/playwright/playwright.config.ts
+++ website/playwright/playwright.config.ts
@@ -8,7 +8,7 @@ const config: PlaywrightTestConfig = {
   testDir: 'tests',
   // testIgnore: '',
   // testMatch: '',
-  timeout: 15000, // fifteen seconds
+  timeout: 10000, // ten seconds
   webServer: {
     env: {
       REACT_APP_BACKEND_URL: 'http://localhost:1337',
